a0f59d695389a9efdc24b041a637ebb1253ca705
Diego Pereira Betti <diegopbetti.gmail.com>
2025-01-14 12:59:38 -0300

    Adiciona imagem de perfil e altera o espacamento entre os componentes

--- index.php
+++ index.php
@@ -6,15 +6,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Meu portifolio</title>
 </head>
-<body class="bg-[url('/images/img1.png')] h-64 w-full bg-cover bg-center">
-    <section class="flex flex-col items-center justify-end h-full text-center">
+<body class="bg-[url('/images/img1.png')] h-screen w-full bg-cover bg-center">
+    <section class="flex flex-col items-center justify-center h-full text-center gap-y-3">
+        <img src="/images/perfil.jpg" alt="" class="object-contain object-cover rounded-full h-24 w-24 border-2 border-rose-500 mb-10">
         <h2 class="text-gray-400">Hello World! Meu nome é <span class="text-red-400" >Goldie</span> e sou</h2>
-        <h1 class="text-white text-3xl font-bold">Desenvolvedor PHP</h1>
+        <h1 class="text-white text-5xl font-bold">Desenvolvedor PHP</h1>
         <p class="text-gray-600 w-3/5">
             Transformo necessidades em aplicações reais, envolventes e funcionais. Desenvolvo sistemas através da minha 
             paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.
         </p>
-        <div class="flex gap-x-3 mt-4">
+        <div class="flex gap-x-3 mt-16">
             <span class="bg-green-400 px-3 py-1 rounded-full">Github</span>
             <span class="bg-violet-400 px-3 py-1 rounded-full">PHP</span>
             <span class="bg-blue-400 px-3 py-1 rounded-full">CSS</span>
