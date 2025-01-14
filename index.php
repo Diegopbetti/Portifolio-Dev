973071799a6d9cc620a049ad72babc670a4df8df
Diego Pereira Betti <diegopbetti.gmail.com>
2025-01-14 10:54:32 -0300

    Adiciona os components que vao ficar na primeira section e os centraliza

--- index.php
+++ index.php
@@ -6,7 +6,21 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Meu portifolio</title>
 </head>
-<body class="bg-[url('/images/img1.png')]">
-    
+<body class="bg-[url('/images/img1.png')] h-64 w-full bg-cover bg-center">
+    <section class="flex flex-col items-center justify-end h-full text-center">
+        <h2 class="text-gray-400">Hello World! Meu nome é <span class="text-red-400" >Goldie</span> e sou</h2>
+        <h1 class="text-white text-3xl font-bold">Desenvolvedor PHP</h1>
+        <p class="text-gray-600 w-3/5">
+            Transformo necessidades em aplicações reais, envolventes e funcionais. Desenvolvo sistemas através da minha 
+            paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.
+        </p>
+        <div class="flex gap-x-3 mt-4">
+            <span class="bg-green-400 px-3 py-1 rounded-full">Github</span>
+            <span class="bg-violet-400 px-3 py-1 rounded-full">PHP</span>
+            <span class="bg-blue-400 px-3 py-1 rounded-full">CSS</span>
+            <span class="bg-red-400 px-3 py-1 rounded-full">HTML</span>
+            <span class="bg-yellow-400 px-3 py-1 rounded-full">JavaScript</span>
+        </div>
+    </section>
 </body>
 </html>
