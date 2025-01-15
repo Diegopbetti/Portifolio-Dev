771bd2a170b2458ca91ef03a79e5b3297d845037
Diego Pereira Betti <diegopbetti.gmail.com>
2025-01-15 19:44:28 -0300

    Adiciona estilizacao no scroll

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="scroll.css">
     <title>Meu portifolio</title>
 </head>
 
-<body>
-    <?php include('./components/header.php') ?>
+<body class="bg-zinc-900">
+<?php include('./components/header.php') ?>
 
-    <?php include('./components/posts.php') ?>
+<?php include('./components/posts.php') ?>
 
-    <?php include('./components/footer.php') ?>
+<?php include('./components/footer.php') ?>  
 </body>
 
 </html>
