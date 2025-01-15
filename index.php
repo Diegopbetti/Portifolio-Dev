d1d99e30f43bf055df96a8231ebf341588395b42
Diego Pereira Betti <diegopbetti.gmail.com>
2025-01-15 11:17:08 -0300

    Adiciona o footer com todos os componentes e sua estilizacao

--- index.php
+++ index.php
@@ -113,6 +113,44 @@
             </div>
         </div>
     </section>
+
+    <footer class="flex flex-col items-center mt-16" >
+        <div>
+            <h3 class="text-purple-400 text-center">Contato</h3>
+            <h2 class="text-white text-2xl text-center" >Gostou do meu trabalho?</h2>
+            <span class="text-gray-400 text-center">Entre em contato ou acompanhe as minhas redes sociais!</span>
+        </div>
+        <div class="mt-10 flex flex-col gap-y-4">
+            <div class="flex bg-gray-800 w-96 h-12 items-center justify-between rounded-xl">
+                <div class="flex ml-4">
+                    <img src="/images/linkedin-logo.png" alt="" class="h-7 w-7">
+                    <span class="text-gray-400 ml-2" >LinkedIn</span>
+                </div>
+                <img src="/images/arrow-up-right.png" alt="" class="text-gray-400 h-7 w-7 mr-4">
+            </div>
+            <div class="flex bg-gray-800 w-96 h-12 items-center justify-between rounded-xl">
+                <div class="flex ml-4">
+                    <img src="/images/instagram-logo.png" alt="" class="h-7 w-7">
+                    <span class="text-gray-400 ml-2" >LinkedIn</span>
+                </div>
+                <img src="/images/arrow-up-right.png" alt="" class="text-gray-400 h-7 w-7 mr-4">
+            </div>
+            <div class="flex bg-gray-800 w-96 h-12 items-center justify-between rounded-xl">
+                <div class="flex ml-4">
+                    <img src="/images/github-logo.png" alt="" class="h-7 w-7">
+                    <span class="text-gray-400 ml-2" >LinkedIn</span>
+                </div>
+                <img src="/images/arrow-up-right.png" alt="" class="text-gray-400 h-7 w-7 mr-4">
+            </div>
+            <div class="flex bg-gray-800 w-96 h-12 items-center justify-between rounded-xl">
+                <div class="flex ml-4">
+                    <img src="/images/envelope-simple.png" alt="" class="h-7 w-7">
+                    <span class="text-gray-400 ml-2" >LinkedIn</span>
+                </div>
+                <img src="/images/arrow-up-right.png" alt="" class="text-gray-400 h-7 w-7 mr-4">
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
